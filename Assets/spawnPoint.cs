ec5362ecea96d084cf1088259c3787503b86cffb
Joe Jalbert <joejalbert29@gmail.com>
2015-05-28 19:32:50 -0400

    Fixed the checkpoint problem and the menu occlusion problem.

---
+++ Assets/spawnPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnPoint : MonoBehaviour {
+
+	public int checkSpawn;
+
+	void Awake () {
+	
+		checkSpawn = PlayerPrefs.GetInt ("CurrentStage");
+		GameObject[] checkPoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+		foreach(GameObject checkpoint in checkPoints){
+			if(checkSpawn == checkpoint.GetComponent<checkPoint>().checkPointNum){
+				transform.position = checkpoint.transform.position;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
