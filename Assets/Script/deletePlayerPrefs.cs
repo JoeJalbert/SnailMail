73b833b6ac6dac40107d5b75ba8c691840fc6396
Joe Jalbert <joejalbert29@gmail.com>
2015-05-29 01:18:05 -0400

    Added Don's newer level select icons. The truck is now a button that removes all progress.

---
+++ Assets/Script/deletePlayerPrefs.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deletePlayerPrefs : MonoBehaviour {
+	
+	public void deleteCheck () {
+		PlayerPrefs.SetInt("CheckpointProgress", 0);
+	}
+}
