d894bc179142f31135fad0105d6042406d960e62
Joe Jalbert <joejalbert29@gmail.com>
2015-06-01 02:11:46 -0400

    Adjusted rolling. Fixed timer, now tracks fastest time. Doggy door functionality, cheese physics.

--- Assets/Script/timeKeep.cs
+++ Assets/Script/timeKeep.cs
@@ -9,15 +9,28 @@ public class timeKeep : MonoBehaviour {
 	public float seconds;
 	public float minutes;
 
-	void Start () {
+	void Awake () {
+		seconds = 0;
+		minutes = 0;
 	}
 
 	void Update () {
 		if(GameObject.FindWithTag("Finish").GetComponent<FinishLine>().youWin == false){
-		time += Time.deltaTime;
+			time += Time.deltaTime;
+		}
+		else if((minutes * 60 + seconds) < PlayerPrefs.GetFloat("BestTime", 9999)){
+			PlayerPrefs.SetFloat ("BestTime", ((minutes * 60) + seconds));
 		}
 		minutes = Mathf.Floor(time/60);
-		seconds = Mathf.RoundToInt (time%60);
-		timer.text = " " + minutes.ToString () + ":" + seconds.ToString () + "";
+		seconds = Mathf.RoundToInt (time%59);
+		if(seconds < 10 && minutes < 10){
+			timer.text = " 0" + minutes.ToString () + ":0" + seconds.ToString () + "";
+		}
+		else if(seconds < 10 && minutes >= 10){
+			timer.text = " " + minutes.ToString () + ":0" + seconds.ToString () + "";
+		}
+		else if(minutes < 10 && seconds >= 10){
+			timer.text = " 0" + minutes.ToString () + ":" + seconds.ToString () + "";
+		}
 	}
 }
